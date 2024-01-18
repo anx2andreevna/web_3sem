273c507aa224bc28a9c9dbfc39863c689bc8296e
Anna Tsvetkova <anx2andreevna@gmail.com>
2024-01-18 21:19:24 +0300

    add tags

--- web3_project/src/App.tsx
+++ web3_project/src/App.tsx
@@ -1,42 +1,51 @@
 import React from 'react'; 
-import { Table } from 'antd'; 
- 
-function App() { 
-  interface IDataItem { 
+import { Table, Tag } from 'antd'; 
+
+const { Column } = Table;
+
+  interface DataType { 
     key: string; 
     name: string; 
     age: number; 
     address: string; 
+    tags: string[];
   } 
  
- 
-  const dataSource: IDataItem[] = [ 
-    { key: '1', name: 'Nanna Christiansen', age: 25, address: 'Street 123' }, 
-    { key: '2', name: 'Sedef Sarıoğlu', age: 30, address: 'Street 456' }, 
-    { key: '3', name: 'Nicoline Christiansen', age: 28, address: 'Avenue 789' }, 
-    { key: '4', name: 'Reina Ramírez', age: 35, address: 'Road 321' }, 
-    { key: '5', name: 'Ege Başoğlu', age: 22, address: 'Lane 654' }, 
-    { key: '6', name: 'Olivia Hokkanen', age: 40, address: 'Boulevard 987' }, 
-    { key: '7', name: 'Radivoy Zavadovskiy', age: 27, address: 'Way 234' }, 
-    { key: '8', name: 'William Martinez', age: 33, address: 'Circle 567' }, 
-    { key: '9', name: 'Chloe Li', age: 29, address: 'Square 890' }, 
-    { key: '10', name: 'Filippo Kehl', age: 26, address: 'Park 432'}, 
+
+  const data: DataType[] = [ 
+    { key: '1', name: 'Nanna Christiansen', age: 25, address: 'Street 123', tags: ['nice', 'developer'], }, 
+    { key: '2', name: 'Sedef Sarıoğlu', age: 30, address: 'Street 456', tags: ['loser'], }, 
+    { key: '3', name: 'Nicoline Christiansen', age: 28, address: 'Avenue 789', tags: ['cool', 'teacher'], }, 
+    { key: '4', name: 'Reina Ramírez', age: 35, address: 'Road 321', tags: ['loves avocado'] }, 
+    { key: '5', name: 'Ege Başoğlu', age: 22, address: 'Lane 654', tags: ['strange'] }, 
+    { key: '6', name: 'Olivia Hokkanen', age: 40, address: 'Boulevard 987', tags: ['partymaker'] }, 
+    { key: '7', name: 'Radivoy Zavadovskiy', age: 27, address: 'Way 234', tags: ['fine', 'shine'] }, 
+    { key: '8', name: 'William Martinez', age: 33, address: 'Circle 567', tags: ['nice'] }, 
+    { key: '9', name: 'Chloe Li', age: 29, address: 'Square 890', tags: ['artist'] }, 
+    { key: '10', name: 'Filippo Kehl', age: 26, address: 'Park 432', tags: ['actor']},
   ]; 
- 
-  const columns = [ 
-    { title: 'Name', dataIndex: 'name', key: 'name' }, 
-    { title: 'Age', dataIndex: 'age', key: 'age' }, 
-    { title: 'Address', dataIndex: 'address', key: 'address' }, 
-  ]; 
-  return ( 
-    <div> 
-      <Table<IDataItem> 
-        dataSource={dataSource} 
-        columns={columns} 
-      /> 
-    </div> 
-  ); 
- 
-} 
+  const App: React.FC = () => (
+    <Table dataSource={data}>
+      
+        <Column title="Name" dataIndex="name" key="name" />
+      <Column title="Age" dataIndex="age" key="age" />
+      <Column title="Address" dataIndex="address" key="address" />
+      <Column
+        title="Tags"
+        dataIndex="tags"
+        key="tags"
+        render={(tags: string[]) => (
+          <>
+            {tags.map((tag) => (
+              <Tag color="green" key={tag}>
+                {tag}
+              </Tag>
+            ))}
+          </>
+        )}
+      />
+    </Table>
+  );
+  
  
 export default App;
