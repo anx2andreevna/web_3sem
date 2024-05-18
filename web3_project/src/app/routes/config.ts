e5f9717a3f978512a5cc124401461b0a5bf5382d
Anna Tsvetkova <anx2andreevna@gmail.com>
2024-05-19 00:42:13 +0300

    dynamic pagination

--- web3_project/src/app/routes/config.ts
+++ web3_project/src/app/routes/config.ts
@@ -1,4 +1,5 @@
-export const ARTEM_ROUTE = 'artem';
+export const HOME_ROUTE = 'home';
+export const DYNAMIC_PAGINATION_ROUTE = 'dynamic_pagination';
 export const MASHA_ROUTE = 'masha';
 export const VLAD_ROUTE = 'vlad';
 export const TABLE_PAGINATION_ROUTE = 'table_pagination';
---
+++ web3_project/src/pages/Pagination/Pagination.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DynamicPagination from "../../components/DynamicPagination";
+
+export const Pagination: React.FC = () => {
+
+    return (    
+        <DynamicPagination>  
+        </DynamicPagination>
+    )
+}
+
+export default Pagination;
