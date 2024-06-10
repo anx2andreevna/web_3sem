ef7610310f2f9b9d2ddaa882f669c3a45012f97c
Anna Tsvetkova <anx2andreevna@gmail.com>
2024-06-10 21:13:28 +0300

    pwa

--- web3_project/src/pages/Masha/Masha.tsx
+++ web3_project/src/pages/Masha/Masha.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const Masha = () => {
   return <>Я Masha</>;
 };
--- web3_project/vite.config.ts
+++ web3_project/vite.config.ts
@@ -1,9 +1,41 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-      plugins: [react()],
+      plugins: [
+            react(),
+            VitePWA({
+               registerType: "prompt",
+               includeAssets: ["logo.ico", "apple-touch-icon.png", "masked-icon.svg"],
+               manifest: {
+                  name: "web_project",
+                  short_name: "web_project",
+                  description: "web_project react",
+                  theme_color: "#ffffff",
+                  start_url: "/",
+                  icons: [
+                        {
+                        src: 'logo192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                        },
+                        {
+                        src: 'logo512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                        },
+                        {
+                        src: 'logo512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                        }
+                  ],
+               },
+            }),
+         ],
       test: {
             globals: true,
             environment: 'jsdom',
